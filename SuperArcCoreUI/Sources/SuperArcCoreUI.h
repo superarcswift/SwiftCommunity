057d11d2e0e65252f2ed783dfe97d7ddf4beb904
An Tran <tran.binhan@gmail.com>
2019-07-16 11:44:27 +0200

    Add SuperArcCoreUI

---
+++ SuperArcCoreUI/Sources/SuperArcCoreUI.h
@@ -0,0 +1,19 @@
+//
+//  SuperArcCoreUI.h
+//  SuperArcCoreUI
+//
+//  Created by An Tran on 16.07.19.
+//  Copyright © 2019 An Tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SuperArcCoreUI.
+FOUNDATION_EXPORT double SuperArcCoreUIVersionNumber;
+
+//! Project version string for SuperArcCoreUI.
+FOUNDATION_EXPORT const unsigned char SuperArcCoreUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SuperArcCoreUI/PublicHeader.h>
+
+
