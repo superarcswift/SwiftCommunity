e00f59dd0a5b909fc8e44f4480cbd73657b2bf23
An Tran <tran.binhan@gmail.com>
2019-07-23 00:03:21 +0200

    Add SuperArcCoordinator

---
+++ SuperArcCoordinator/Sources/Top/SuperArcCoordinator.h
@@ -0,0 +1,19 @@
+//
+//  SuperArcCoordinator.h
+//  SuperArcCoordinator
+//
+//  Created by An Tran on 22.07.19.
+//  Copyright © 2019 An Tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SuperArcCoordinator.
+FOUNDATION_EXPORT double SuperArcCoordinatorVersionNumber;
+
+//! Project version string for SuperArcCoordinator.
+FOUNDATION_EXPORT const unsigned char SuperArcCoordinatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SuperArcCoordinator/PublicHeader.h>
+
+
--- SwiftVideos/Sources/Application/Scenes/Dashboard/DashboardCoordinator.swift
+++ SwiftVideos/Sources/Application/Scenes/Dashboard/DashboardCoordinator.swift
@@ -2,6 +2,7 @@
 //  Copyright © 2019 An Tran. All rights reserved.
 //
 
+import SuperArcCoordinator
 import SuperArcCoreUI
 import SuperArcCore
 import RxSwift
