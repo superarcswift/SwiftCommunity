1a46c2314433122bd1fb10414b28dd568fb43372
peacemoon <tran.binhan@gmail.com>
2019-08-14 00:51:51 +0200

    Add Component template

---
+++ Tools/Xcode-Templates/SuperArc/Component.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,37 @@
+//
+//___COPYRIGHT___
+//
+
+import SuperArcCoreComponent
+import SuperArcCoreUI
+import SuperArcCore
+import SuperArcFoundation
+import XCoordinator
+
+/// Protocol defining all dependencies required by this component.
+typealias VideosDependency = HasVideosService
+
+class ___FILEBASENAMEASIDENTIFIER___: Component<VideosDependency>, VideosBuilder {
+
+    // MARK: Properties
+
+    // Static
+
+    // IBOutlets
+
+    // Public
+
+    // Private
+
+    // MARK: Setup
+
+    override func setupViews() {
+        super.setupViews()
+    }
+
+    override func setupBindings() {
+        super.setupBindings()
+    }
+
+    // MARK: Private helpers
+}
