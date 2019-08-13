0905303970d026e4cc02eeb5864e98db7408b2dc
An Tran <tran.binhan@gmail.com>
2019-08-14 00:49:37 +0200

    Add Streaming player (#15)

--- SwiftVideos/Sources/Application/Scenes/Videos/Player/ViewController/VideoPlayerViewController.swift
+++ SwiftVideos/Sources/Application/Scenes/Videos/Player/ViewController/VideoPlayerViewController.swift
@@ -5,6 +5,7 @@
 import SuperArcCoreUI
 import SuperArcCore
 import YoutubeKit
+import Player
 
 class VideoPlayerViewController: ViewController<VideoPlayerViewModel>, StoryboardInitiable {
 
@@ -20,7 +21,8 @@ class VideoPlayerViewController: ViewController<VideoPlayerViewModel>, Storyboar
 
     // Public
 
-    private var player: YTSwiftyPlayer!
+    private var youtubePlayer: YTSwiftyPlayer!
+    private var streamingPlayer: Player!
 
     // MARK: Overrides
 
@@ -28,15 +30,16 @@ class VideoPlayerViewController: ViewController<VideoPlayerViewModel>, Storyboar
 
         super.setupViews()
 
-        if case let .youtube(id) = viewModel.videoMetaData.source {
-            player = YTSwiftyPlayer(frame: view.frame, playerVars: [.videoID(id)])
-            player.autoplay = false
-            playerContainerView.addAndStretchSubView(player)
-            player.delegate = self
-            player.loadPlayer()
-
-            navigationController?.setNavigationBarHidden(true, animated: false)
+        switch viewModel.videoMetaData.source {
+        case .youtube(let id):
+            setupYoutubePlayer(withVideoID:  id)
+        case .wwdc(let url):
+            setupStreamingPlayer(withVideoURL: url)
+        default:
+            fatalError("video source not supported")
         }
+
+        navigationController?.setNavigationBarHidden(true, animated: false)
     }
 
     override func viewWillDisappear(_ animated: Bool) {
@@ -48,7 +51,27 @@ class VideoPlayerViewController: ViewController<VideoPlayerViewModel>, Storyboar
     @IBAction func didTapClose(_ sender: Any) {
         navigationController?.popViewController(animated: true)
     }
+
+    //  MARK: Private helpers
+
+    private func setupYoutubePlayer(withVideoID id: String) {
+        youtubePlayer = YTSwiftyPlayer(frame: view.frame, playerVars: [.videoID(id)])
+        youtubePlayer.autoplay = false
+        playerContainerView.addAndStretchSubView(youtubePlayer)
+        youtubePlayer.delegate = self
+        youtubePlayer.loadPlayer()
+    }
+
+    private func setupStreamingPlayer(withVideoURL urlString: String)  {
+        streamingPlayer = Player()
+        playerContainerView.addAndStretchSubView(streamingPlayer.view)
+        streamingPlayer.didMove(toParent: self)
+
+        if let url = URL(string: urlString) {
+            streamingPlayer.url = url
+            streamingPlayer.playFromBeginning()
+        }
+    }
 }
 
-extension VideoPlayerViewController: YTSwiftyPlayerDelegate {
-}
+extension VideoPlayerViewController: YTSwiftyPlayerDelegate {}
