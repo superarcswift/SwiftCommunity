a21c7d57f4ccbeba794874d0ffa156fb905f290f
peacemoon <tran.binhan@gmail.com>
2019-11-17 17:19:35 +0100

    Deploy updates

---
+++ Tools/build_landingpage.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+directory=_site
+branch=gh-pages
+build_command() {
+  bundler exec jekyll build
+}
+
+pushd "web/landingpage"
+
+echo -e "\033[0;32mDeleting old content...\033[0m"
+rm -rf $directory
+
+echo -e "\033[0;32mChecking out $branch....\033[0m"
+git worktree add $directory $branch
+
+echo -e "\033[0;32mGenerating site...\033[0m"
+build_command
+
+echo -e "\033[0;32mDeploying $branch branch...\033[0m"
+cd $directory &&
+  git add --all &&
+  git commit -m "Deploy updates" &&
+  git push origin $branch
+
+echo -e "\033[0;32mCleaning up...\033[0m"
+git worktree remove $directory
+
+popd
