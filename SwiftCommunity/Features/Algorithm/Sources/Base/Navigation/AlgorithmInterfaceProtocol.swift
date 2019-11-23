25043bdeb6df1cf267f6c3ec062b6b6705d547db
peacemoon <tran.binhan@gmail.com>
2019-11-23 01:33:32 +0100

    update

---
+++ SwiftCommunity/Features/Algorithm/Sources/Base/Navigation/AlgorithmInterfaceProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  Copyright © 2019 An Tran. All rights reserved.
+//
+
+import Core
+import DataModels
+import SuperArcCoreComponent
+import SuperArcCoreUI
+import SuperArcCore
+import UIKit
+
+public protocol AlgorithmInterfaceProtocol: Interface {
+    func showDashboard() -> UIViewController
+
+}
---
+++ SwiftCommunity/Features/Algorithm/Sources/Base/Scenes/Dashboard/DashboardViewController.swift
@@ -0,0 +1,9 @@
+//
+//  Copyright © 2019 An Tran. All rights reserved.
+//
+
+import SuperArcCoreUI
+
+class DashboardViewController: ViewController<ViewModel>, StoryboardInitiable {
+    static var storyboardName = "Dashboard"
+}
