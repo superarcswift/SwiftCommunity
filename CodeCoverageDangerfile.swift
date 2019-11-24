fa4e6a543ebba72810d23bb6a6bcc703aa7d7a11
An Tran <tran.binhan@gmail.com>
2019-11-24 22:57:15 +0100

    Add danger code coverage (#123)

---
+++ CodeCoverageDangerfile.swift
@@ -0,0 +1,8 @@
+import Danger
+import DangerSwiftCoverage
+import Foundation
+
+// Gather coverage for modified files
+Coverage.xcodeBuildCoverage(.derivedDataFolder("./build/DerivedData"),
+                            minimumCoverage: 50,
+                            excludedTargets: [])
